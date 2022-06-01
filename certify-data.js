5b24296cdc6a18a460753ff8af92b085b03ba097
Cristina Castro <cristinacastro87@gmail.com>
2022-06-01 15:26:25 +0200

    Integrate BLOOCK SDK

--- certify-data.js
+++ certify-data.js
@@ -3,42 +3,41 @@ const { BloockClient, Record } = require("@bloock/sdk");
 module.exports = function (RED) {
   function certifyData(config) {
     RED.nodes.createNode(this, config);
-    this.bloock = RED.nodes.getNode(n.bloock);
-
-    if (!this.bloock || !this.bloock.credentials.apikey) {
-      this.warn(RED._("Previous authentication is required"));
-      return;
-    }
-
-    const apiKey = "";
-    const client = this.bloock.credentials.apikey && new BloockClient(apiKey);
+    const apiKey = process.env.API_KEY;
+    const client = new BloockClient(apiKey);
     let node = this;
 
-    node.on("input", function (msg) {
-      let data = msg.payload;
+    node.on("input", async function (msg) {
+      let data = config.data;
+
       if (data) {
         switch (typeof data) {
           case "string":
-            data = Record.fromString(data);
+            console.log("string");
+            data = await Record.fromString(data);
             break;
           case "object":
-            data = Record.fromJSON(JSON.parse(data));
+            console.log("object");
+            data = await Record.fromJSON(JSON.parse(data));
             break;
           case "array":
-            data = Record.fromTypedArray(data);
+            console.log("array");
+            data = await Record.fromTypedArray(data);
             break;
           default:
             console.log(`${typeof data} is not a valid type of data.`);
         }
       }
 
-      let preparedData = client.sendRecords(data).then((result) => {
-        return { anchor: result[0].anchor };
-      });
-
-      msg.payload = preparedData;
-      node.send(msg);
+      client
+        .sendRecords([data])
+        .then((result) => {
+          node.send({ anchor: result[0].anchor });
+        })
+        .catch((err) => {
+          node.send(err);
+        });
     });
   }
-  RED.nodes.registerType("certify-data", certifyData);
+  RED.nodes.registerType("certifyData", certifyData);
 };
--- test/certify-data_spec.js
+++ test/certify-data_spec.js
@@ -2,14 +2,27 @@ const helper = require("node-red-node-test-helper");
 const certifyDataNode = require("../certify-data.js");
 const bloockNode = require("../bloock.js");
 
+afterEach(function () {
+  helper.unload();
+});
+// separat secret credentials object to be passed in at launch, adhering to how nodered protects secrets
+
+
+console.log(process.env.API_KEY)
+
 const credentials = {
-  apikey: "",
+  apikey:
+    "test_8cq2kKTtGOsHm32oUwukf8NQ46Ozxg62aB4gfZTf6HQWPfzwsB1vM3-k0Sksh8x4",
 };
+
+// the way nodered wants to define flows and relations, and initial values
 const flow = [
   {
     id: "n1",
     type: "certify-data",
-    name: "test data",
+    displayName: "test data",
+    apikey:
+      "test_8cq2kKTtGOsHm32oUwukf8NQ46Ozxg62aB4gfZTf6HQWPfzwsB1vM3-k0Sksh8x4",
     wires: [["n2"]],
   },
   {
@@ -18,28 +31,50 @@ const flow = [
   },
 ];
 
-describe("certify-data Node", function () {
-  afterEach(function () {
-    helper.unload();
-  });
-
-  it("should have credentials", function (done) {
+describe("Node load with config", function () {
+  
+  // node should be loaded fine in the runtime
+  it("should be loaded", function (done) {
     helper.load([certifyDataNode, bloockNode], flow, credentials, function () {
+      console.log("h");
       const n1 = helper.getNode("n1");
-      n1.credentials.should.have.property("name", "test data");
+      n1.should.have.property("displayName", "test data");
       done();
     });
   });
 
+  // Node should have needed credentials
+  it("should have credentials", function (done) {
+    console.log("he");
+
+    helper.load([certifyDataNode, bloockNode], flow, credentials, function () {
+      const n1 = helper.getNode("n1");
+      console.log(n1, "es aioc?");
+      n1.credentials.should.have.property(
+        "apikey",
+        "test_8cq2kKTtGOsHm32oUwukf8NQ46Ozxg62aB4gfZTf6HQWPfzwsB1vM3-k0Sksh8x4"
+      );
+      console.log("df")
+      done();
+    });
+  });
+});
+
+describe("Node certifying data", function () {
+  //node should certify data
   it("should certify data", function (done) {
+    console.log("hel");
+
     try {
+      console.log("hell");
+
       helper.load(
         [certifyDataNode, bloockNode],
         flow,
         credentials,
         function () {
-          const n2 = helper.getNode("n2");
           const n1 = helper.getNode("n1");
+          const n2 = helper.getNode("n2");
           n2.on("input", function (msg) {
             msg.payload.should.have.property("error", false);
             msg.should.have.property("payload", "timestamp");
